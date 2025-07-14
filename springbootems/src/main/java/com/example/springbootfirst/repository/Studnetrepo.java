045ec548b88c81c82fd6160f5317adf34c4d4354
sharan560 <sharannagarajan06@gmail.com>
2025-07-14 20:46:58 +0530

    day10

--- springbootems/src/main/java/com/example/springbootfirst/repository/Studnetrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootfirst.repository;
-
-import com.example.springbootfirst.services.Studentseivice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Studnetrepo extends JpaRepository<Studentseivice,Integer> {
-}
