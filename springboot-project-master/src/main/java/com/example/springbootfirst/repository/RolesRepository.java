7fafe53e8aab23b79f3518aa69dc9bccff814e81
sharan560 <sharannagarajan06@gmail.com>
2025-07-14 17:49:44 +0530

    docker added

--- springboot-project-master/src/main/java/com/example/springbootfirst/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootfirst.repository;
-
-import com.example.springbootfirst.models.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RolesRepository extends JpaRepository<Roles,Integer> {
-    Optional<Roles> findByRoleName(String roleName);
-}
