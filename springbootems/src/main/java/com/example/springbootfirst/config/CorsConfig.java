87c134c2f491e409b22bddbe52d35e6f89846da8
sharan560 <sharannagarajan06@gmail.com>
2025-07-17 19:35:58 +0530

    Cors issue resolved

--- springbootems/src/main/java/com/example/springbootfirst/config/CorsConfig.java
+++ springbootems/src/main/java/com/example/springbootfirst/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://springboot-intern-rjt3.onrender.com")
+                .allowedOrigins("http://localhost:5173", "https://ems-react-ten.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
